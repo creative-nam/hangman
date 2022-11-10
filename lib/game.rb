668dba7341345108cfe727e3feb14f1d851d37d2
creative-username <myc82570@gmail.com>
2022-11-10 21:41:34 +0200

    Process the guess provided by the player
    Implemented the logic to process the guess made by the player, that is -
    evaluate whether the guess is correct, and put add it to appropriate
    list; and determine if the player has won.

--- lib/game.rb
+++ lib/game.rb
@@ -12,14 +12,14 @@ class Game
 
   @@max_amount_of_guesses = 12
 
-  attr_accessor :word_to_be_guessed, :guesses_left, :incorrect_guesses, :letters_guessed
+  attr_accessor :word_to_be_guessed, :guesses_left, :incorrect_guesses, :correct_guesses
 
   def initialize(_word_to_guessed = nil, _incorrect_guesses = nil)
     self.word_to_be_guessed = generate_random_word
     self.guesses_left = @@max_amount_of_guesses
 
     self.incorrect_guesses = []
-    self.letters_guessed = []
+    self.correct_guesses = []
   end
 
   def display_word
@@ -38,15 +38,19 @@ class Game
 
     until guess_valid?(guess)
       puts incorrect_guesses_msg(incorrect_guesses) unless incorrect_guesses.empty?
-
       puts guesses_left_msg(guesses_left)
 
-      puts guess_prompt_msg(guesses_left)
-      guess = gets.chomp
-      guess = guess[0] if guess.length > 1
+      puts guess_prompt_msg
+      guess = gets.chomp[0]
 
       puts invalid_guess_msg unless guess_valid?(guess)
     end
+
+    process_guess(guess)
+  end
+
+  def player_won?
+    (word_to_be_guessed.chars - correct_guesses).empty?
   end
 
   def decorate_line
@@ -58,20 +62,43 @@ class Game
   private
 
   def guessed?(letter)
-    letters_guessed.include?(letter)
+    correct_guesses.include?(letter)
   end
 
   def guess_valid?(guess)
     guess && guess.match?(/[[:alpha:]]/)
+  end
+
+  def correct?(guess)
+    word_to_be_guessed.include?(guess)
+  end
+
+  def process_guess(guess)
+    if correct?(guess)
+      correct_guesses << guess
+    else
+      incorrect_guesses << guess
+
+      decrement_guesses_left
+    end
+  end
+
+  def decrement_guesses_left
+    self.guesses_left -= 1
   end
 end
 
 game = Game.new
 puts "Word to be guessed: #{game.word_to_be_guessed}"
 
-until game.guesses_left.zero? || game.letters_guessed.length == game.word_to_be_guessed.length
+until game.guesses_left.zero? || game.player_won?
   puts "Word: #{game.display_word}"
 
   game.decorate_line
   game.prompt_for_guess
 end
+
+puts "Word to be guesses: #{game.word_to_be_guessed.chars}"
+puts "Letters guessed: #{game.correct_guesses}"
+puts "Won? #{game.player_won?}"
+puts game.player_won? ? 'You won!' : 'You lost!'
